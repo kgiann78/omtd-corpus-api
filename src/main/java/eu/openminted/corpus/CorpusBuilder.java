31e7c4e351c1a6b3588751212165377e43023c75
kgiann78 <kgiann78@gmail.com>
2017-08-04 18:51:38 +0300

    Code refactor: * Add ServiceStatus * Add additional method for content api * FetchMetadataTask: add fulltextLimit checking even for documents that don't have hashkeys * CorpusBuilderImple: code * application.properties: removing unnecessary properties and change those used in docker swarm * corpus-builder-config: add dev-override.properties for those properties that are only needed in development * Dockerfile: add system variable that gives the name of the override properties file

--- src/main/java/eu/openminted/corpus/CorpusBuilder.java
+++ src/main/java/eu/openminted/corpus/CorpusBuilder.java
@@ -4,33 +4,33 @@ import eu.openminted.content.connector.Query;
 import eu.openminted.registry.domain.Corpus;
 
 public interface CorpusBuilder {
-    /***
+    /**
      * Standard method to prepare building a corpus (gathers necessary information)
      * @param query Search information
      * @return Corpus containing the corpus metadata
      */
     Corpus prepareCorpus(Query query);
 
-    /***
+    /**
      * Standard method to begin building the corpus
      * @param corpusMetadata metadata stored for the corpus building
      */
     void buildCorpus(Corpus corpusMetadata);
 
-    /***
+    /**
      * Returns the status of a particular corpus based on its ID
      * @param corpusID corpus ID
      * @return CorpusStatus
      */
     CorpusStatus getStatus(String corpusID);
 
-    /***
+    /**
      * Standard method to recall the building of a corpus.
      * @param corpusID corpus ID
      */
     void cancelProcess(String corpusID);
 
-    /***
+    /**
      * Standard method to delete a created corpus based on its ID
      * @param corpusID corpus ID
      */
